98dcad0d329a2ec4b5682c6917f52c427fe3d699
randominte <149210151+randominte@users.noreply.github.com>
2025-11-15 15:54:16 -0600

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -9,10 +9,10 @@ addLayer("n", {
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "numerals", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "ideas", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0.48, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
